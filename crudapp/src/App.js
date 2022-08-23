d0b6560ffa2aec242654cf4f9798a3aab9bdb97e
Palash Hawee <haweepalash@gmail.com>
2022-08-23 14:16:12 +0530

    Edit and View codes

--- crudapp/src/App.js
+++ crudapp/src/App.js
@@ -6,6 +6,9 @@ import Contact from "./components/pages/Contact";
 import Navbar from "./components/layout/Navbar";
 import NotFound from "./components/pages/NotFound";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddUser from "./components/users/AddUser";
+import EditUser from "./components/users/EditUser";
+import User from "./components/users/User";
 function App() {
   return (
     <Router>
@@ -15,6 +18,9 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/users/add" element={<AddUser />} />
+          <Route exact path="/users/edit/:id" element={<EditUser />} />
+          <Route exact path="/users/:id" element={<User />} />
           <Route exact path="*" element={<NotFound />} />
         </Routes>
       </div>
